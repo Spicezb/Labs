4d239d96dd3a45b9abca3425d50a2f59387b2ff3
Luisguialfa <luisguialfa@gmail.com>
2025-05-04 22:04:42 -0600

    inicio

---
+++ labMatrices/alquiler.py
@@ -0,0 +1,4 @@
+# Trabajo realizado por Luis Guillermo ALfaro y Xavier Cespedes Alvarado
+# Fecha de creación 04/05/2025 21:45
+#
+# Versión de python 3.13.2
---
+++ labMatrices/funciones.py
@@ -0,0 +1,14 @@
+# Trabajo realizado por Luis Guillermo ALfaro y Xavier Cespedes Alvarado
+# Fecha de creación 04/05/2025 21:45
+#
+# Versión de python 3.13.2
+
+# Libreiras
+
+# Funciones
+def menu():
+    x=0
+    while x != 0:
+        print("1) Alquilar apartamento\n" \
+            "2) Modificar renta\n" \
+            "3) Desalojar")
