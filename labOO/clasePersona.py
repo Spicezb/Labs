92edded2af91f623b80b6292ef7e80eac0b10321
Luisguialfa <luisguialfa@gmail.com>
2025-05-21 18:50:33 -0600

    cuentame el final, el final final feliz

---
+++ labOO/clasePersona.py
@@ -0,0 +1,52 @@
+class Persona:
+# Definicion de atributos
+    nombre=""
+    cedula=""
+    categoria=""
+    profesion=""
+    estado=True
+# Definicion de los metodos
+    def __init__(self):
+        self.nombre=""
+        self.cedula=""
+        self.categoria=""
+        self.profesion=""
+        self.estado=True
+    
+    def setNombre(self, pnombre):
+        self.nombre=pnombre
+        return self.nombre
+    
+    def setCedula(self, pcedula):
+        self.cedula=pcedula
+        return self.cedula
+    
+    def setCategoria(self, pcategoria):
+        self.categoria=pcategoria
+        return self.categoria
+    
+    def setProfesion(self, pprofesion):
+        self.profesion=pprofesion
+        return self.profesion
+    
+    def setEstado(self, pestado):
+        self.estado=pestado
+        return self.estado
+    
+    def getNombre(self):
+        return self.nombre
+    
+    def getCedula(self):
+        return self.cedula
+    
+    def getCategoria(self):
+        return self.categoria
+    
+    def getProfesion(self):
+        return self.profesion
+    
+    def getEstado(self):
+        return self.estado
+    
+    def getDatos(self):
+        return f"Cédula: {self.cedula}, Nombre: {self.nombre}, Categoría: {self.categoria}, Profesión: {self.profesion}, Estado: {self.estado}"
--- labOO/main.py
+++ labOO/main.py
@@ -5,7 +5,9 @@
 
 #Importación de librerías
 import re
+import random
 from funciones import *
+from clasePersona import*
 
 #Definición de funciones
 def modificarNombreAux(lista):
@@ -40,6 +42,82 @@ def modificarNombreAux(lista):
         except:
             print("Debe ingresar un nombre con dos apellidos.")
     if confirmar()==True:
-        modificarNombre(objeto,nombre)
+        modificar(objeto,nombre,1)
         return "El nombre fue modificado"
     return "La acción fue cancelada y el nombre no se modificó."
+
+def eliminarAux(lista):
+    """
+    Funcionamiento:
+    - Valida el nombre y la cédula del miembro que se requiere modificar.
+    Entradas:
+    - lista(list): Es la lista que contiene todos los objetos.
+    Salidas:
+    - Llama a la función principal y retorna un str según corresponda.
+    """
+    cedulas=[]
+    while True:
+        try:
+            cedula=input("Ingrese la cédula del miembro que desea eliminar: ")
+            if not re.match(r"[1-9]\d{8}$",cedula):
+                raise ValueError
+            for i in lista:
+                cedulas.append(i.getCedula())
+            if cedula not in cedulas:
+                raise ValueError
+            objeto=lista[cedulas.index(cedula)]
+            if objeto.getEstado()==True:
+                if confirmar()==True:
+                    modificar(objeto,False,2)
+                    return "El miembro fue eliminado."
+            break
+        except ValueError:
+            print("La cédula ingresada no se encuentra registrada.")
+    return "La acción fue cancelada y el miembro no se eliminó."
+
+def insertarMiembro(lista,archivo):
+    """
+    Funcionamiento:
+    - Se le pide al ususario la cantidad de miembros de trabajo, para crearlos ficticiamente.
+    - Se crea la cantidad de personas indicada.
+    Entradas:
+    - lista(list): Es la lista que contiene todos los objetos.
+    - archivo(str): Es el nombre del archivo donde se guarda la lista.
+    Salidas:
+    - Retorna un mensaje indicando que se agregaron crearon exitosamente.
+    """
+    lstCed=[]
+    lstTrabajos=["Software Developer","Analyst","Engineer","Game designer","Web designer","Designer","Game programmer","Webmaster",
+                "Web developer","Network administrator","Software Engineer","Scientist","Video game developer","Data Engineer",
+                "Strategist","Web Application Developer","Java Developer"]
+    while True:
+        try:
+            cantMiembros=int(input("Ingrese la cantidad de personas en el equiopo de trabajo: "))
+            if cantMiembros<=0:
+                raise TypeError
+            break
+        except ValueError:
+            print("Debe ingresar un valor númerico\n")
+        except TypeError:
+            print("Debe ingresar un valor mayor a 0\n")
+    for i in range(cantMiembros):
+        miembro=Persona()
+        nombre=crearNombres()
+        cedula=random.randint(100000000,999999999)
+        if cedula in lstCed:
+            while cedula in lstCed:
+                cedula=random.randint(100000000,999999999)
+        lstCed.append(cedula)
+        categoria=random.randint(1,4)
+        perso=random.randint(1,4)
+        trab=random.randint(0,len(lstTrabajos)-1)
+        persoDefini=determinarPerso(categoria,perso)
+
+        miembro.setNombre(nombre)
+        miembro.setCedula(str(cedula))
+        miembro.setCategoria([categoria,persoDefini])
+        miembro.setProfesion(lstTrabajos[trab])
+        miembro.setEstado(random.choice([True,False]))
+        annadir(miembro,lista,archivo)
+
+    return print("Los miembros se crearon exitosamente.")
