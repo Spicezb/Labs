fad9eca4203bcbf5ff00f5e237103be269b1295e
Spicezb <xaviccr@gmail.com>
2025-06-10 19:32:46 -0600

    reto 2

---
+++ labRecuCola/funciones.py
@@ -0,0 +1,20 @@
+# Elaborado por: Xavier Céspedes Alvarado y Luis GUillermo Alfaro Chacón.
+# Fecha de creación: 10/06/2025 19:00
+# Última modificación: 10/06/2025 21:30
+# Versión: 3.13.3
+
+# Definición de funciones
+
+# Reto 2. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+def obtenersumCuadrados(m,n,res=0):
+    if m == n:
+        return res + n**2
+    res += m**2 
+    return obtenersumCuadrados(m+1,n,res)
+
+def obtenerSumCuadradosAux(m,n):
+    if n<m:
+        return -1
+    if type(n)!= int or type(m)!=int:
+        return -1
+    return obtenersumCuadrados(m,n)
---
+++ labRecuCola/menu.py
@@ -0,0 +1,4 @@
+# Elaborado por: Xavier Céspedes Alvarado y Luis GUillermo Alfaro Chacón.
+# Fecha de creación: 10/06/2025 19:00
+# Última modificación: 10/06/2025 21:30
+# Versión: 3.13.3
