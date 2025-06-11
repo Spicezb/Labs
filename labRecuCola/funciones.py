30a0c668896d1c32146a929a4e1c42190c138e2d
Luisguialfa <luisguialfa@gmail.com>
2025-06-11 02:26:29 -0600

    reto 5

--- labRecuCola/funciones.py
+++ labRecuCola/funciones.py
@@ -26,3 +26,43 @@ def esBinario(num):
     if num==0:
         return True
     return esBinario(num//10)
+
+# reto 5. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+def contarBisiestos(pNum1,pNum2,conta):
+    """
+    Funcionamiento:
+    - Recorre recursivamente el rango de años entre pNum1 y pNum2 y cuenta cuántos de esos años son bisiestos.
+    Entradas:
+    - pNum1 (int): Año inicial del rango.
+    - pNum2 (int): Año final del rango.
+    - conta (int): Acumulador que lleva la cuenta de los años bisiestos encontrados.
+    Salidas:
+    - Retorna un entero con la cantidad total de años bisiestos entre pNum1 y pNum2 (inclusive).
+    """
+    if pNum1<=pNum2:
+        if pNum1%4==0 and ((pNum1%100!=0) or (pNum1%400==0)):
+            pNum1+=1
+            conta+=1
+            return contarBisiestos(pNum1,pNum2,conta)
+        pNum1+=1    
+        return contarBisiestos(pNum1,pNum2,conta)
+    return conta
+
+def contarBisiestosAux(pNum1,pNum2):
+    """
+    Funcionamiento:
+    - Verifica que ambos valores ingresados sean enteros positivos.
+    - Además, valida que el año inicial sea menor o igual al año final.
+    - Si todo es válido, llama a la función recursiva 'contarBisiestos' para contar los años bisiestos en ese rango.
+    Entradas:
+    - pNum1 (int): Año inicial del rango.
+    - pNum2 (int): Año final del rango.
+    Salidas:
+    - Retorna un mensaje de error si alguno de los valores no es válido.
+    - Si todo es correcto, retorna la cantidad de años bisiestos entre pNum1 y pNum2 (inclusive).
+    """
+    if (type(pNum1)!=int or pNum1<0) or (type(pNum2)!=int or pNum2<0):
+        return "Los valores deben de ser números enteros positivos mayores a 0."
+    elif pNum1>pNum2:
+        return "El año inicial debe ser debe ser menor que el año final."
+    return contarBisiestos(pNum1,pNum2,0)
--- labRecuCola/menu.py
+++ labRecuCola/menu.py
@@ -11,38 +11,33 @@ from funciones import *
 
 # Reto 2. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 print("\n***** Reto 2. Suma de cuadrados. *****")
-x,y = 4,7
-print(f"\nPara la entrada: {x,y}")
-print("Resultado:")
-print(obtenerSumCuadradosAux(x,y))
-x,y = 4.5,7
-print(f"\nPara la entrada: {x,y}")
-print("Resultado:")
-print(obtenerSumCuadradosAux(x,y))
-x,y = 7,4
-print(f"\nPara la entrada: {x,y}")
-print("Resultado:")
-print(obtenerSumCuadradosAux(x,y))
-x,y = 1,5
-print(f"\nPara la entrada: {x,y}")
-print("Resultado:")
-print(obtenerSumCuadradosAux(x,y))
+print(f"\nPara la entrada: {4,7}\nResultado:")
+print(obtenerSumCuadradosAux(4,7))
+print(f"\nPara la entrada: {4.5,7}\nResultado:")
+print(obtenerSumCuadradosAux(4.5,7))
+print(f"\nPara la entrada: {7,4}\nResultado:")
+print(obtenerSumCuadradosAux(7,4))
+print(f"\nPara la entrada: {1,5}\nResultado:")
+print(obtenerSumCuadradosAux(1,5))
 
 # Reto 4. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 print("\n***** Reto 4. Validar binario. *****")
-x = 1001019
-print(f"\nPara la entrada: {x}")
-print("Resultado:")
-print(esBinario(x))
-x = 0
-print(f"\nPara la entrada: {x}")
-print("Resultado:")
-print(esBinario(x))
-x = 5679
-print(f"\nPara la entrada: {x}")
-print("Resultado:")
-print(esBinario(x))
-x = 10101
-print(f"\nPara la entrada: {x}")
-print("Resultado:")
-print(esBinario(x))
+print(f"\nPara la entrada: {1001019}\nResultado:")
+print(esBinario(1001019))
+print(f"\nPara la entrada: {0}\nResultado:")
+print(esBinario(0))
+print(f"\nPara la entrada: {5679}\nResultado:")
+print(esBinario(5679))
+print(f"\nPara la entrada: {10101}\nResultado:")
+print(esBinario(10101))
+
+# Reto 5. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+print("\n***** Reto 5. Contar años Bisiestos. *****")
+print(f"\nPara la entrada: {1500,1836}\nResultado:")
+print(contarBisiestosAux(1500,1836))
+print(f"\nPara la entrada: {2000,2025}\nResultado:")
+print(contarBisiestosAux(2000,2025))
+print(f"\nPara la entrada: {2022,2023}\nResultado:")
+print(contarBisiestosAux(2022,2023))
+print(f"\nPara la entrada: {2023,2020}\nResultado:")
+print(contarBisiestosAux(2023,2020))
