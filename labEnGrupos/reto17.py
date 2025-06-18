c95406dd52ba60fa8eaae877696fdc58045cb47f
Spicezb <xaviccr@gmail.com>
2025-06-18 16:17:59 -0600

    reto 17 listo

--- labEnGrupos/reto17.py
+++ labEnGrupos/reto17.py
@@ -1,9 +1,13 @@
 def analizarNotas(lista,listaT=[]):
+    esta=False
     if lista==[]:
         return listaT
-    for i,j,k in enumerate(listaT):
-        if j==lista[0]:
-            listaT[i][1]+=1
+    for i,j in enumerate(listaT):
+        if j[0]==lista[0]:
+            esta=True
+            listaT[i]=(j[0],j[1]+1)
+    if esta==False:
+        listaT.append((lista[0],1))
     return analizarNotas(lista[1:],listaT)
 
-analizarNotas([100, 875, 480, 560, 480, 875, 100, 100, 575, 480, 480])
+print(analizarNotas([100, 875, 480, 560, 480, 875, 100, 100, 575, 480, 480]))
